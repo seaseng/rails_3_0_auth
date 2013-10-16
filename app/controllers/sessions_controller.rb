00dd6197fe60dd93e27e48ca853111bfe5d0f0e8
Navid Mostafavi <navidm@gmail.com>
2013-10-16 03:29:02 -0700

    adding all necessary migrations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      if params[:remember_me]
+        # session[:user_id] = user.id
+        # user auth_token instead of session hash for more permanent storage
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
       redirect_to root_url, notice: "Logged In"
     else
       flash.now.alert = "Email or password is invalid"
@@ -14,7 +20,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    # session[:user_id] = nil
+    cookies.delete(:auth_token)
     redirect_to root_url, :notice => "Logged Out."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails30Auth::Application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  root :to => "session#new"
+
   resources :users
   resources :sessions
+  resources :password_resets
 end
